215299ff247c6ee2e04c22680ab0a125b67c71ba
Muhammad Nouman <NoumanHumayun@gmail.com>
2022-02-07 04:44:50 +0500

    update DB name var

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ dotenv.config();
 module.exports = app = {
     secret: process.env.JWT_SECRET,
     port: parseInt(process.env.PORT) || 8080,
-    dbName: process.env.DB,
+    dbName: process.env.DB_NAME,
     dbUser: process.env.DB_USER,
     dbPass: process.env.DB_PASS,
     dbHost: process.env.DB_HOST
